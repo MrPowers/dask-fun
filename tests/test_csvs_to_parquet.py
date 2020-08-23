70d9088ed05efc9572f4e21212d88c700e7a9d70
MrPowers <matthewkevinpowers@gmail.com>
2020-08-23 16:40:58 -0500

    Read CSV files and write out Parquet files

--- tests/test_csvs_to_parquet.py
+++ tests/test_csvs_to_parquet.py
@@ -2,5 +2,13 @@ import pytest
 
 from dask_examples.csvs_to_parquet import *
 
-def test_convert_csvs_to_parquet():
-    convert_csvs_to_parquet()
+def test_convert_csvs_to_parquet2():
+    convert_csvs_to_parquet2()
+
+
+def test_convert_csvs_to_parquet4():
+    convert_csvs_to_parquet4()
+
+
+def test_convert_csvs_to_snappy_parquet():
+    convert_csvs_to_snappy_parquet()
